ff81e4b9534d7b3a7a4d4262ffc55646558c0fc6
Robert Egeland <robegeland@yahoo.com>
2022-08-04 20:41:45 -0500

    commit tree 18c279b5baa047665b58199f35bf5f2b29fea69c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+    validate :fiction_or_nonfiction
+
+    def fiction_or_nonfiction
+        unless category == "Fiction" || category == "Non-Fiction"
+            errors.add(:category, "is not included in the list")
+        end
+    end
 end
